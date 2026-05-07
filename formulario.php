829cf66890b622b90e720827216cd81aa453c0d6
THEfoxde <thefoxde2005@gmail.com>
2026-05-07 18:36:24 -0300

    Archivos necesarios y estructura base HTML

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 8 - Grupo 6</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="contenedor">
+        <h2>Ingreso de Serie Preferida</h2>
+    <!-- aca va el commit de Facu Aguilar -->
+    </div>
+</body>
+</html>
