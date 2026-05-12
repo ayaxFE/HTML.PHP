5120f6de4194ce7cce47cc49f88118d248f338f3
Luis M. Díaz <84762039+lmdiaz-dv@users.noreply.github.com>
2026-05-12 17:23:48 -0300

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -14,4 +14,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 ?>
- <!--<aca hace el commit Luis>-->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado - Grupo 6</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="contenedor">
+        <?php if ($mensaje_error != ""): ?>
+            <p class="error"><?php echo $mensaje_error; ?></p>
+        <?php else: ?>
+            <p>Tu serie preferida es: <b style="color: red;"><i><?php echo $serie_ingresada; ?></i></b>.</p>
+        <?php endif; ?>
+        
+        <a href="formulario.php" class="boton-volver">Volver al inicio</a>
+    </div>
+</body>
+</html>
+
